4644306cc52dd55368681f0d2c5c228ebca48b24
Sathaam99 <98530549+Sathaam99@users.noreply.github.com>
2026-06-17 23:34:59 +0530

    Change server port from 5000 to 5060

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def health_check():
 
 if __name__ == '__main__':
     # Binds to 0.0.0.0 so it's accessible outside the container
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5060)
